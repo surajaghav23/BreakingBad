d75afed781d816f0608aec35804176291000e8d7
Suraj Aghav <dineshaghav123@gmail.com>
2020-09-23 23:51:35 +0530

    Initial commit

---
+++ src/components/ui/Search.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Serach = ({ getQuery }) => {
+  const [text, setText] = useState('');
+
+  const onChange = (q) => {
+    setText(q);
+    getQuery(q);
+  };
+
+  return (
+    <section className='search'>
+      <form action=''>
+        <input
+          type='text'
+          className='fprm-control'
+          placeholder='Search characters'
+          value={text}
+          onChange={(e) => {
+            onChange(e.target.value);
+          }}
+          autoFocus
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Serach;
